5ab5f6d4e593be35082846ca764055d4c12bf255
CodeSandbox <support@codesandbox.io>
2025-04-05 19:05:50 +0000

    Sandbox snapshot commit

--- web-dev/src/components/Home.jsx
+++ web-dev/src/components/Home.jsx
@@ -1,38 +1,64 @@
 import React from "react";
-import { TERipple } from "tw-elements-react";
 
-export default function Home() {
+const cards = [
+  {
+    image: "",
+    title: "title1",
+    discription: "this this my first card",
+    button: "Learn More",
+  },
+  {
+    image: "",
+    title: "title2",
+    discription: "this this my second card",
+    button: "Learn More",
+  },
+  {
+    image: "",
+    title: "title3",
+    discription: "this this my third card",
+    button: "Learn More",
+  },
+  {
+    image: "",
+    title: "title4",
+    discription: "this this my fourth card",
+    button: "Learn More",
+  },
+  {
+    image: "",
+    title: "title5",
+    discription: "this this my fifth card",
+    button: "Learn More",
+  },
+];
+
+const Card = ({ images, title, discription, button }) => {
   return (
-    <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <TERipple>
-        <div className="relative overflow-hidden bg-cover bg-no-repeat">
-          <img
-            className="rounded-t-lg"
-            src="https://tecdn.b-cdn.net/img/new/standard/nature/186.jpg"
-            alt=""
-          />
-          <a href="#!">
-            <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
-          </a>
-        </div>
-      </TERipple>
-      <div className="p-6">
-        <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-          Card title
-        </h5>
-        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <TERipple>
-          <button
-            type="button"
-            className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-          >
-            Button
-          </button>
-        </TERipple>
+    <div className="max-w-sm rounded overflow-hidden shdaow-lg flex">
+      <div className="px-6 py-4 border-2 border-slate-300 rounded-lg">
+        <img src={images} alt="" />
+        <div className="text-2xl font-bold mb-2">{title}</div>
+        <p className="text-slate-700 text-">{discription}</p>
+        <button>{button}</button>
       </div>
     </div>
   );
-}
+};
+
+const Home = () => {
+  return (
+    <div className="flex flex-wrap justify-center">
+      {cards.map((card, index) => (
+        <Card
+          key={index}
+          image={card.image}
+          title={card.title}
+          discription={card.discription}
+          button={card.button}
+        />
+      ))}
+    </div>
+  );
+};
+export default Home;
