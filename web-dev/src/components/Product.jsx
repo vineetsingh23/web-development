02cfe67b1ae0cd8efd6a42266dbb1553c337f2ba
CodeSandbox <support@codesandbox.io>
2025-04-06 01:17:39 +0000

    Sandbox snapshot commit

--- web-dev/src/components/Product.jsx
+++ web-dev/src/components/Product.jsx
@@ -1,18 +1,39 @@
 import React from "react";
+import { Carousel } from "@material-tailwind/react";
 const Product = () => {
   return (
-    <div className="min-h-screen bg-slate-50 w-full flex items-center justify-center">
-      <div className="flex flex-col items-start max-w-3xl gap-5">
-        <h1 className="text-6xl font-bold text-black text-center mt-20">
-          Product Page
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-          deleniti repudiandae cupiditate nulla, possimus id repellendus velit!
-          Repellat, asperiores quaerat.
-        </p>
-      </div>
-    </div>
+    <Carousel
+      className="rounded-xl"
+      navigation={({ setActiveIndex, activeIndex, length }) => (
+        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+          {new Array(length).fill("").map((_, i) => (
+            <span
+              key={i}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+              }`}
+              onClick={() => setActiveIndex(i)}
+            />
+          ))}
+        </div>
+      )}
+    >
+      <img
+        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+        alt="image 1"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        alt="image 2"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+        alt="image 3"
+        className="h-full w-full object-cover"
+      />
+    </Carousel>
   );
 };
 
